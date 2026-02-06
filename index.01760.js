cec25697f7278e446161a8b0554e9afc58fd8d0f
thesalt0818 <thesalt0818@gmail.com>
2026-02-06 18:05:29 +0900

    공격 스킬 이펙트 추가
    공격 스킬 이펙트 추가

--- index.01760.js
+++ index.01760.js
@@ -1,4 +1,4 @@
-System.register(["./application.f38c1.js"], function (_export, _context) {
+System.register(["./application.5b671.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
