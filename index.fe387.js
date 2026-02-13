373645259b76a568ee4655b6a244c3fc235d1d2f
thesalt0818 <thesalt0818@gmail.com>
2026-02-13 18:17:56 +0900

    일꾼 추가

--- index.fe387.js
+++ index.fe387.js
@@ -1,4 +1,4 @@
-System.register(["./application.cfaeb.js"], function (_export, _context) {
+System.register(["./application.2fdf2.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
