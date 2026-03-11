b4a259605f5ae4d684cf79ff7b07eae013bdb218
thesalt0818 <thesalt0818@gmail.com>
2026-03-11 14:49:04 +0900

    사운드추가

--- application.f751a.js
+++ application.f751a.js
@@ -14,7 +14,7 @@ System.register([], function (_export, _context) {
       _export("Application", Application = /*#__PURE__*/function () {
         function Application() {
           _classCallCheck(this, Application);
-          this.settingsPath = 'src/settings.1e4b9.json';
+          this.settingsPath = 'src/settings.71028.json';
           this.showFPS = false;
         }
         _createClass(Application, [{
