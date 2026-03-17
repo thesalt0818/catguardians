4402b3b4ed24e4e1c23ab00dc65d01d0120faab4
thesalt0818 <thesalt0818@gmail.com>
2026-03-17 15:50:17 +0900

    이펙트 추가

--- index.6acfe.js
+++ index.6acfe.js
@@ -1,4 +1,4 @@
-System.register(["./application.664c1.js"], function (_export, _context) {
+System.register(["./application.e4603.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
