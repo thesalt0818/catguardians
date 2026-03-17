6111ed2500f6188d9e3998b8c4362afc8b90b555
thesalt0818 <thesalt0818@gmail.com>
2026-03-17 18:06:34 +0900

    터치 사운드 추가

--- index.d48fe.js
+++ index.d48fe.js
@@ -1,4 +1,4 @@
-System.register(["./application.e4603.js"], function (_export, _context) {
+System.register(["./application.8ebb0.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
